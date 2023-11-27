de626aea74237c6bd5f7077d070c3d7e27dcd30e
satonohime <nananariru@gmail.com>
2023-11-27 12:22:13 -0800

    add yaml loader for table creation/deletion

--- drop_tables_mysql.py
+++ drop_tables_mysql.py
@@ -1,6 +1,23 @@
 import mysql.connector
-db_conn = mysql.connector.connect(host="vm1-3855-a01031389.westus.cloudapp.azure.com", user="user",
-password="password", database="events")
+import yaml
+import os
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+db_conn = mysql.connector.connect(
+    host=app_config["datastore"]["hostname"], 
+    user=app_config["datastore"]["user"],
+    password=app_config["datastore"]["password"], 
+    database=app_config["datastore"]["db"]
+)
+
 db_cursor = db_conn.cursor()
 db_cursor.execute('''
     DROP TABLE event, event_cancel
